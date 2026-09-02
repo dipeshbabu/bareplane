fc916adfa4321f30fdd762f28251b126b52e0ce1
Dipesh Babu <59379458+dipeshbabu@users.noreply.github.com>
2026-09-02 16:11:24 -0400

    Add config validation command

--- internal/cli/cli.go
+++ internal/cli/cli.go
@@ -3,8 +3,10 @@ package cli
 import (
 	"fmt"
 	"io"
+	"os"
 	"strings"
 
+	"github.com/dipeshbabu/bareplane/internal/config"
 	"github.com/dipeshbabu/bareplane/internal/version"
 )
 
@@ -14,6 +16,7 @@ Usage:
   bareplane <command>
 
 Commands:
+  validate   Validate bareplane.yaml
   version    Print build version information
   help       Show this help text
 `
@@ -31,8 +34,38 @@ func Run(args []string, stdout, stderr io.Writer) int {
 	case "version", "-v", "--version":
 		fmt.Fprintln(stdout, version.String())
 		return 0
+	case "validate":
+		return runValidate(args[1:], stdout, stderr)
 	default:
 		fmt.Fprintf(stderr, "unknown command %q\n\n%s", args[0], usage)
 		return 2
 	}
+}
+
+func runValidate(args []string, stdout, stderr io.Writer) int {
+	if len(args) > 1 {
+		fmt.Fprintln(stderr, "usage: bareplane validate [path]")
+		return 2
+	}
+
+	path := "bareplane.yaml"
+	if len(args) == 1 {
+		path = args[0]
+	}
+
+	file, err := os.Open(path)
+	if err != nil {
+		fmt.Fprintf(stderr, "open %s: %v\n", path, err)
+		return 1
+	}
+	defer file.Close()
+
+	cfg, err := config.Load(file)
+	if err != nil {
+		fmt.Fprintf(stderr, "%s: %v\n", path, err)
+		return 1
+	}
+
+	fmt.Fprintf(stdout, "%s is valid for cluster %q\n", path, cfg.Metadata.Name)
+	return 0
 }
